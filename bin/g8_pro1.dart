6b8054d4732394f4432d9ccd042028fead08b88d
6631501048 <6631501048@lamduan.mfu.ac.th>
2025-08-25 19:04:42 +0700

    initial commit

---
+++ bin/g8_pro1.dart
@@ -0,0 +1,13 @@
+import 'package:g8_pro1/g8_pro1.dart' as g8_pro1;
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+import 'dart:convert';
+
+//fea 1+2
+
+//fea 3
+
+//fea 4
+
+//fea 5+6
---
+++ test/g8_pro1_test.dart
@@ -0,0 +1,8 @@
+import 'package:g8_pro1/g8_pro1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
